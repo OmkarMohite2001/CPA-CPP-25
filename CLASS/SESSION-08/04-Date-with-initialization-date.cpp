7d6aeba5dc6fd6735bdbced8f499dc67c8a40bd9
OmkarMohite2001 <omkarmohite85@gmail.com>
2026-05-25 12:03:52 +0530

    session 08

---
+++ CLASS/SESSION-08/04-Date-with-initialization-date.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Date
+{
+    private:
+        int day;
+        int month;
+        int year;
+
+    public:
+        Date(int init_day,int init_month,int init_year)
+        {
+
+        }
+};
+
+int main(void)
+{
+    
+}
